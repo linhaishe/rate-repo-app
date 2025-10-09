a4c0e6fb0c8389bdfa4adc3eff76a3944ec97167
linhaishe <42030043+linhaishe@users.noreply.github.com>
2025-10-09 11:24:56 +0800

    style: fix style for nav

--- components/AppBar.jsx
+++ components/AppBar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import Text from './styleComponent/Text';
@@ -5,32 +6,43 @@ import { Link } from 'react-router-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 const styles = StyleSheet.create({
-  bgColor: {
-    backgroundColor: '#24292e',
-  },
   container: {
     paddingTop: Constants.statusBarHeight,
+  },
+  navLinkWrap: {
+    alignItems: 'center',
+    backgroundColor: '#24292e',
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 20,
+    height: 30,
+    justifyContent: 'center',
+    width: '100%',
+    // border: '1px solid red',
+    // flex: 1,
   },
 });
 
 const AppBar = () => {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container} edges={['top']}>
-        <ScrollView horizontal>
-          <View style={styles.bgColor}>
-            {/* <Pressable onPress={() => console.log(' we are press')}> */}
-            <Link to={'/'}>
-              <Text color={'primary'}>Repository</Text>
-            </Link>
-            {/* </Pressable> */}
-            <Link to={'/signin'}>
-              <Text color={'primary'}>SignIn</Text>
-            </Link>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    // <SafeAreaProvider>
+    //   <SafeAreaView style={styles.container} edges={['top']}>
+    //     <ScrollView horizontal contentContainerStyle={{ flexGrow: 1 }}>
+    <View style={styles.navLinkWrap}>
+      <Link to={'/'}>
+        <Text style={{ color: '#fff' }} fontWeights={'bold'}>
+          Repository
+        </Text>
+      </Link>
+      <Link to={'/signin'}>
+        <Text style={{ color: '#fff' }} fontWeights={'bold'}>
+          SignIn
+        </Text>
+      </Link>
+    </View>
+    //     </ScrollView>
+    //   </SafeAreaView>
+    // </SafeAreaProvider>
   );
 };
 
